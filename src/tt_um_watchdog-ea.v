25105adbf79efbee98b17663bce31d1d22ade848
Lukas Rabeder <s2210306017@fhooe.at>
2025-11-10 13:06:18 +0100

    added enable logic

--- src/tt_um_watchdog-ea.v
+++ src/tt_um_watchdog-ea.v
@@ -2,14 +2,14 @@ module tt_um_watchdog
 (
     input  logic clk,
     input  logic rst_n,
+    input  logic ena,
     input  logic [7:0] ui_in,       // User Input (7 bits)
     input  logic [7:0] uio,       // User InOut (7 bits)
     output logic [7:0] uo_out      // User Output (8 bits)
 );
-
-    // Internal Signals
     logic clk_i;
     logic rst_n_i;
+
     logic [7:0] ui_in_i;
     logic [7:0] uio_i;
     logic [7:0] uo_out_i;
@@ -31,7 +31,24 @@ module tt_um_watchdog
     assign ui_in_i = {1'b0, ui_in};  // Pad ui_in to 8 bits
     assign uio_i = {1'b0, uio};    // Pad uio to 8 bits
     assign uo_out = uo_out_i;
-
+always_ff @(posedge clk or negedge rst_n) begin
+  if (!rst_n) 
+  begin
+    alpha <= 32'b0;
+    beta  <= 32'b0;
+    invK  <= 32'b0;
+    K     <= 32'b0;
+    regime <= 3'b000;
+    uo_out <= 8'b0;
+    core_busy <= 1'b0;
+    res_valid <= 1'b0;
+    ol_busy <= 1'b0;
+    start_ol <= 1'b0;
+    eig_core_start <= 1'b0;
+  end 
+  else 
+  if (ena) 
+  begin 
     // param_loader Instance
     param_loader u_pl (
         .clk(clk_i),
@@ -68,5 +85,12 @@ module tt_um_watchdog
         .busy(ol_busy),
         .out_byte(uo_out_i)
     );
+  end
+  else
+  begin
+    uo_out <= 8'bit0;
+  end
+end
+
 
 endmodule
